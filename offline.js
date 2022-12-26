a49b9a2169e52c50521436f10f280bebef12393e
Natsu-Infinito <108439839+Natsu-Infinito@users.noreply.github.com>
2022-12-26 01:13:29 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1672026954,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/inv_background-sheet0.png",
+		"images/inv_exitbutton-sheet0.png",
+		"images/inv_slot-sheet0.png",
+		"images/player_box-sheet0.png",
+		"images/obj_item-sheet0.png",
+		"images/obj_bau-sheet0.png",
+		"images/obj_bau-sheet1.png",
+		"images/obj_bau-sheet2.png",
+		"images/ground.png",
+		"images/sombra-sheet0.png",
+		"images/player_anim-sheet0.png",
+		"images/player_anim-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
